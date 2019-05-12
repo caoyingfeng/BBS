0f9bc04e4a29e4bfeb07ad993f31baac73048935
caoyingfeng <164972038@qq.com>
2019-05-12 23:26:29 +0800

    cms_models_migrate_upgrade

--- bbs.py
+++ bbs.py
@@ -3,15 +3,21 @@ from apps.cms import bp as cms_bp
 from apps.common import bp as common_bp
 from apps.front import bp as front_bp
 import config
+from exts import db
 
 
-app = Flask(__name__)
-app.config.from_object(config)
-app.register_blueprint(cms_bp)
-app.register_blueprint(common_bp)
-app.register_blueprint(front_bp)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(config)
 
+    app.register_blueprint(cms_bp)
+    app.register_blueprint(common_bp)
+    app.register_blueprint(front_bp)
+
+    db.init_app(app)
+    return app
 
 
 if __name__ == '__main__':
+    app = create_app()
     app.run()
