753eb9e245d82e424f3b0051c683f7a256b170fc
caoyingfeng <164972038@qq.com>
2019-05-17 23:02:49 +0800

    add CMSRole and permission

--- apps/cms/models.py
+++ apps/cms/models.py
@@ -2,6 +2,44 @@ from exts import db
 from datetime import datetime
 from werkzeug.security import generate_password_hash,check_password_hash
 
+
+class CMSPersmission(object):
+    # 255的二进制方式来表示 1111 1111
+    ALL_PERMISSION = 0b11111111
+    # 1. 访问者权限
+    VISITOR =        0b00000001
+    # 2. 管理帖子权限
+    POSTER =         0b00000010
+    # 3. 管理评论的权限
+    COMMENTER =      0b00000100
+    # 4. 管理板块的权限
+    BOARDER =        0b00001000
+    # 5. 管理前台用户的权限
+    FRONTUSER =      0b00010000
+    # 6. 管理后台用户的权限
+    CMSUSER =        0b00100000
+    # 7. 管理后台管理员的权限
+    ADMINER =        0b01000000
+
+
+cms_role_user = db.Table(
+    'cms_role_user',
+    db.Column('cms_role_id',db.Integer,db.ForeignKey('cms_role.id'),primary_key=True),
+    db.Column('cms_user_id',db.Integer,db.ForeignKey('cms_user.id'),primary_key=True)
+)
+
+
+class CMSRole(db.Model):
+    __tablename__ = 'cms_role'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50), nullable=False)
+    desc = db.Column(db.String(200),nullable=True)
+    create_time = db.Column(db.DateTime,default=datetime.now)
+    permissions = db.Column(db.Integer,default=CMSPersmission.VISITOR)
+
+    users = db.relationship('CMSUser',secondary=cms_role_user,backref='roles')
+
+
 class CMSUser(db.Model):
     __tablename__ = 'cms_user'
     id = db.Column(db.Integer,primary_key=True,autoincrement=True)
@@ -26,3 +64,21 @@ class CMSUser(db.Model):
     def check_password(self,raw_password):
         result = check_password_hash(self.password,raw_password)
         return result
+
+    @property
+    def permissions(self):
+        if not self.roles:
+            return 0
+        all_permissions = 0
+        for role in self.roles:
+            permission = role.permissions
+            all_permissions |= permission
+        return all_permissions
+
+    def has_permission(self,permission):
+        return self.permissions & permission == permission
+
+    @property
+    def is_developer(self):
+        return self.has_permission(CMSPersmission.ALL_PERMISSION)
+
