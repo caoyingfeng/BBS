803f324b6df5b0d02d47b43ca69766c6bb86eae4
caoyingfeng <164972038@qq.com>
2019-06-02 20:57:12 +0800

    publish comment

--- static/front/js/front_pdetail.js
+++ static/front/js/front_pdetail.js
@@ -20,3 +20,30 @@ $(function () {
     });
     window.ue = ue;
 });
+
+$(function () {
+    $("#comment-btn").click(function (event) {
+        event.preventDefault();
+        var loginTag = $("#login-tag").attr("data-is-login");
+        if(!loginTag){
+            window.location = '/signin/';
+        }else{
+            var content = window.ue.getContent();
+            var post_id = $("#post-content").attr("data-id");
+            myajax.post({
+                'url': '/acomment/',
+                'data':{
+                    'content': content,
+                    'post_id': post_id
+                },
+                'success': function (data) {
+                    if(data['code'] == 200){
+                        window.location.reload();
+                    }else{
+                        myalert.alertInfo(data['message']);
+                    }
+                }
+            });
+        }
+    });
+});
